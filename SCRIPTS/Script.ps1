d941276234a43cd4df7c5dc15ead403fd5c329a3
Alex-0293 <1928311@tuta.io>
2020-04-18 18:13:35 +0300

    Init commit.

---
+++ SCRIPTS/Script.ps1
@@ -0,0 +1,47 @@
+<#
+    .SYNOPSIS 
+        Creator
+        dd.MM.yyyy
+        Ver
+    .DESCRIPTION
+    .PARAMETER
+    .EXAMPLE
+#>
+$ImportResult = Import-Module AlexkUtils  -PassThru
+if ($null -eq $ImportResult) {
+    Write-Host "Module 'AlexkUtils' does not loaded!" -ForegroundColor Red
+    exit 1
+}
+else {
+    $ImportResult = $null
+}
+#requires -version 3
+
+#########################################################################
+function Get-WorkDir () {
+    if ($PSScriptRoot -eq "") {
+        if ($PWD -ne "") {
+            $MyScriptRoot = $PWD
+        }        
+        else {
+            Write-Host "Where i am? What is my work dir?"
+        }
+    }
+    else {
+        $MyScriptRoot = $PSScriptRoot
+    }
+    return $MyScriptRoot
+}
+# Error trap
+trap {
+    Get-ErrorReporting $_    
+    exit 1
+}
+#########################################################################
+Clear-Host
+
+[string]$MyScriptRoot = Get-WorkDir
+[string]$Global:ProjectRoot = Split-Path $MyScriptRoot -parent
+
+Get-VarsFromFile    "$ProjectRoot\VARS\Vars.ps1"
+Initialize-Logging   $ProjectRoot  "Latest"
---
+++ VARS/Vars-empty.ps1
@@ -0,0 +1,6 @@
+#rename this file to Vars.ps1 
+    [string] $ProjectPath = ""
+    [string] $GlobalVars  = ""
+    [string] $Helpers     = ""
+#### Script params
+    [string]$global:LogFilePath          = "$ProjectPath\LOGS\project.log"
