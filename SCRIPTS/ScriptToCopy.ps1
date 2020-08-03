192408748cb30c69a3b69997a5b27b3c2804182f
AlexK <1928311@tuta.io>
2020-08-03 19:01:21 +0300

    Add mermaid support.

---
+++ SCRIPTS/ScriptToCopy.ps1
@@ -0,0 +1,60 @@
+<#
+    .NOTE
+        .AUTHOR %Author%
+        .DATE   %Date%
+        .VER    %Ver%
+        .LANG   %Lang%
+        
+    .LINK
+        %ProjectURL%
+    
+    .COMPONENT
+        %Component%
+
+    .SYNOPSIS 
+
+    .DESCRIPTION
+        %Description% 
+
+    .PARAMETER
+
+    .EXAMPLE
+        %Example%
+
+#>
+Param (
+    [Parameter( Mandatory = $false, Position = 0, HelpMessage = "Initialize global settings." )]
+    [bool] $InitGlobal = $true,
+    [Parameter( Mandatory = $false, Position = 1, HelpMessage = "Initialize local settings." )]
+    [bool] $InitLocal  = $true   
+)
+
+$Global:ScriptInvocation = $MyInvocation
+if ($env:AlexKFrameworkInitScript){
+    . "$env:AlexKFrameworkInitScript" -MyScriptRoot (Split-Path $PSCommandPath -Parent) -InitGlobal $InitGlobal -InitLocal $InitLocal
+} Else {
+    Write-host "Environmental variable [AlexKFrameworkInitScript] does not exist!" -ForegroundColor Red
+     exit 1
+}
+if ($LastExitCode) { exit 1 }
+
+# Error trap
+trap {
+    if (get-module -FullyQualifiedName AlexkUtils) {
+        Get-ErrorReporting $_
+        . "$GlobalSettingsPath\$SCRIPTSFolder\Finish.ps1" 
+    }
+    Else {
+        Write-Host "[$($MyInvocation.MyCommand.path)] There is error before logging initialized. Error: $_" -ForegroundColor Red
+    }   
+    exit 1
+}
+################################# Script start here #################################
+
+
+
+
+
+
+################################# Script end here ###################################
+. "$GlobalSettingsPath\$SCRIPTSFolder\Finish.ps1" 
